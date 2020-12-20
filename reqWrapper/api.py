31364d0b4af8591446909b3e97ab94211a11361d
box-archived <box.cassette@gmail.com>
2020-12-20 09:36:48 +0900

    Fix request with Session

--- reqWrapper/api.py
+++ reqWrapper/api.py
@@ -18,7 +18,6 @@ def request(method, url, session=None, retry=5, wait=1, need_200=False, **kwargs
         try_count += 1
         if try_count != 1:
             sleep(wait)
-        print("try #%s" % try_count)
         try:
             res = session.request(method=method, url=url, **kwargs)
             session.close()
@@ -32,12 +31,14 @@ def request(method, url, session=None, retry=5, wait=1, need_200=False, **kwargs
     return SafeResponse(success=False)
 
 
-def get(url, params=None, retry=5, wait=1, need_200=False, **kwargs):
+def get(url, params=None, session=None, retry=5, wait=1, need_200=False, **kwargs):
     r"""Sends a GET request.
 
     :param url: URL for the new :class:`Request` object.
     :param params: (optional) Dictionary, list of tuples or bytes to send
         in the query string for the :class:`Request`
+    :param session: request with session
+    :type session: requests.Session
     :param retry: Max retry count.
     :param wait: Time(seconds) between retry
     :param need_200: Set True if you only need status code 200
@@ -48,13 +49,15 @@ def get(url, params=None, retry=5, wait=1, need_200=False, **kwargs):
     """
 
     kwargs.setdefault('allow_redirects', True)
-    return request('get', url, params=params, retry=retry, wait=wait, need_200=need_200, **kwargs)
+    return request('get', url, params=params, session=session, retry=retry, wait=wait, need_200=need_200, **kwargs)
 
 
-def options(url, retry=5, wait=1, need_200=False, **kwargs):
+def options(url, session=None, retry=5, wait=1, need_200=False, **kwargs):
     r"""Sends an OPTIONS request.
 
     :param url: URL for the new :class:`Request` object.
+    :param session: request with session
+    :type session: requests.Session
     :param retry: Max retry count.
     :param wait: Time(seconds) between retry
     :param need_200: Set True if you only need status code 200
@@ -65,13 +68,15 @@ def options(url, retry=5, wait=1, need_200=False, **kwargs):
     """
 
     kwargs.setdefault('allow_redirects', True)
-    return request('options', url, retry=retry, wait=wait, need_200=need_200, **kwargs)
+    return request('options', url, session=session, retry=retry, wait=wait, need_200=need_200, **kwargs)
 
 
-def head(url, retry=5, wait=1, need_200=False, **kwargs):
+def head(url, session=None, retry=5, wait=1, need_200=False, **kwargs):
     r"""Sends a HEAD request.
 
     :param url: URL for the new :class:`Request` object.
+    :param session: request with session
+    :type session: requests.Session
     :param retry: Max retry count.
     :param wait: Time(seconds) between retry
     :param need_200: Set True if you only need status code 200
@@ -84,16 +89,18 @@ def head(url, retry=5, wait=1, need_200=False, **kwargs):
     """
 
     kwargs.setdefault('allow_redirects', False)
-    return request('head', url, retry=retry, wait=wait, need_200=need_200, **kwargs)
+    return request('head', url, session=session, retry=retry, wait=wait, need_200=need_200, **kwargs)
 
 
-def post(url, data=None, json=None, retry=5, wait=1, need_200=False, **kwargs):
+def post(url, data=None, json=None, session=None, retry=5, wait=1, need_200=False, **kwargs):
     r"""Sends a POST request.
 
     :param url: URL for the new :class:`Request` object.
     :param data: (optional) Dictionary, list of tuples, bytes, or file-like
         object to send in the body of the :class:`Request`.
     :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param session: request with session
+    :type session: requests.Session
     :param retry: Max retry count.
     :param wait: Time(seconds) between retry
     :param need_200: Set True if you only need status code 200
@@ -103,16 +110,19 @@ def post(url, data=None, json=None, retry=5, wait=1, need_200=False, **kwargs):
     :rtype: reqWrapper.SafeResponse
     """
 
-    return request('post', url, data=data, json=json, retry=retry, wait=wait, need_200=need_200, **kwargs)
+    return request('post', url, data=data, json=json, session=session,
+                   retry=retry, wait=wait, need_200=need_200, **kwargs)
 
 
-def put(url, data=None, retry=5, wait=1, need_200=False, **kwargs):
+def put(url, data=None, session=None, retry=5, wait=1, need_200=False, **kwargs):
     r"""Sends a PUT request.
 
     :param url: URL for the new :class:`Request` object.
     :param data: (optional) Dictionary, list of tuples, bytes, or file-like
         object to send in the body of the :class:`Request`.
     :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param session: request with session
+    :type session: requests.Session
     :param retry: Max retry count.
     :param wait: Time(seconds) between retry
     :param need_200: Set True if you only need status code 200
@@ -122,16 +132,18 @@ def put(url, data=None, retry=5, wait=1, need_200=False, **kwargs):
     :rtype: reqWrapper.SafeResponse
     """
 
-    return request('put', url, data=data, retry=retry, wait=wait, need_200=need_200, **kwargs)
+    return request('put', url, data=data, session=session, retry=retry, wait=wait, need_200=need_200, **kwargs)
 
 
-def patch(url, data=None, retry=5, wait=1, need_200=False, **kwargs):
+def patch(url, data=None, session=None, retry=5, wait=1, need_200=False, **kwargs):
     r"""Sends a PATCH request.
 
     :param url: URL for the new :class:`Request` object.
     :param data: (optional) Dictionary, list of tuples, bytes, or file-like
         object to send in the body of the :class:`Request`.
     :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param session: request with session
+    :type session: requests.Session
     :param retry: Max retry count.
     :param wait: Time(seconds) between retry
     :param need_200: Set True if you only need status code 200
@@ -141,20 +153,22 @@ def patch(url, data=None, retry=5, wait=1, need_200=False, **kwargs):
     :rtype: reqWrapper.SafeResponse
     """
 
-    return request('patch', url, data=data, retry=retry, wait=wait, need_200=need_200, **kwargs)
+    return request('patch', url, data=data, session=session, retry=retry, wait=wait, need_200=need_200, **kwargs)
 
 
-def delete(url, retry=5, wait=1, need_200=False, **kwargs):
+def delete(url, session=None, retry=5, wait=1, need_200=False, **kwargs):
     r"""Sends a DELETE request.
 
     :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :param session: request with session
+    :type session: requests.Session
     :param retry: Max retry count.
     :param wait: Time(seconds) between retry
     :param need_200: Set True if you only need status code 200
         else -> fail
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
     :return: :class:`Response <Response>` object
     :rtype: reqWrapper.SafeResponse
     """
 
-    return request('delete', url, retry=retry, wait=wait, need_200=need_200, **kwargs)
+    return request('delete', url, session=session, retry=retry, wait=wait, need_200=need_200, **kwargs)
