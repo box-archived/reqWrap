b8038557cfbdd7f298098ee8f00a16e23a9d4c60
box-archived <box.cassette@gmail.com>
2020-12-20 09:56:27 +0900

    Reformat repeated imports

--- reqWrapper/__init__.py
+++ reqWrapper/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import requests as requests
-from requests import utils
-from requests import packages
-from requests import Request, Response, PreparedRequest
-from requests import session, Session
-from requests import codes
+from requests import (
+    utils, packages, Request, Response,
+    PreparedRequest, session, Session, codes
+)
 from requests.exceptions import (
     RequestException, Timeout, URLRequired,
     TooManyRedirects, HTTPError, ConnectionError,
