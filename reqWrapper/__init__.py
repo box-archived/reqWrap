1cfcbb2e90dc3b12a5896700a3c53899975b099c
box-archived <box.cassette@gmail.com>
2020-12-20 09:36:16 +0900

    Add import requests' functions

--- reqWrapper/__init__.py
+++ reqWrapper/__init__.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import requests as requests
+from requests import utils
+from requests import packages
+from requests import Request, Response, PreparedRequest
+from requests import session, Session
+from requests import codes
+from requests.exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError,
+    FileModeWarning, ConnectTimeout, ReadTimeout
+)
+
 from .api import request, get, options, head, post, put, patch, delete
 from .model import SafeResponse
-import requests as requests
