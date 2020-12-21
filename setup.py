1dfaa61fb53b5eade34de2e992a8a674f2697ec3
box-archived <box.cassette@gmail.com>
2020-12-21 14:46:04 +0900

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='reqWrapper',
-    version='0.1.0',
+    version='0.1.1',
     packages=['reqWrapper'],
     url='https://github.com/box-archived/reqWrap',
     license='Apache License 2.0',
